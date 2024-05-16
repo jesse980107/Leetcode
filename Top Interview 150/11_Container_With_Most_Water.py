1472ba5300a0859be5159767b44e1fd57c60cb7d
Muxi Jin <jmx1998@student.ubc.ca>
2024-05-15 18:29:07 -0700

    11

--- Top Interview 150/11_Container_With_Most_Water.py
+++ Top Interview 150/11_Container_With_Most_Water.py
@@ -1,2 +1,17 @@
 #Medium
 #Two Pointers
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l=0
+        r = len(height)-1
+        max_v=0
+        while l <r:
+            h = min(height[l],height[r])
+            v= h* (r-l)
+            max_v = max(max_v,v)
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+        return max_v
