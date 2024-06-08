51eb6067b2af5f544abcd77b8d906f281c927cb2
Muxi Jin <jmx1998@student.ubc.ca>
2024-06-07 19:01:05 -0700

    238

--- Top Interview 150/238_Product_of_Array_Except_Self.py
+++ Top Interview 150/238_Product_of_Array_Except_Self.py
@@ -1,15 +1,37 @@
 #Medium
 #Array / String
+
+# if no 0 contain in nums:
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        rst = [1] * len(nums)
-
-        pre = 1
+        total = 1
         for i in range(len(nums)):
-            rst[i] = pre
-            pre *= nums[i]
-        post = 1
-        for i in range(len(nums)-1,-1,-1):
-            rst[i] *= post
-            post*=nums[i]
-        return rst
+            total *= nums[i]
+        ans = [int(total/i) for i in nums]
+        return ans
+    
+# for arbitrary nums:
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        if nums.count(0) >= 2:
+            return [0 for i in nums]
+        elif nums.count(0) == 1:
+            ans = []
+            total = 1
+            for i in range(len(nums)):
+                if nums[i] != 0:
+                    total *= nums[i]
+            for i in range(len(nums)):
+                if nums[i] != 0:
+                    ans.append(0)
+                else:
+                    ans.append(total)
+            return ans
+        else:
+            total = 1
+            for i in range(len(nums)):
+                total *= nums[i]
+            ans = [int(total/i) for i in nums]
+            return ans
+        
+
