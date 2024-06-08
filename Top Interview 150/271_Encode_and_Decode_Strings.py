33846af23d72af3209cbb23f11dfb767a0e1f7ae
Muxi Jin <jmx1998@student.ubc.ca>
2024-06-07 18:27:40 -0700

    271

--- Top Interview 150/271_Encode_and_Decode_Strings.py
+++ Top Interview 150/271_Encode_and_Decode_Strings.py
@@ -1,2 +1,11 @@
 #Medium
 #Array / String
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+
+        delimiter = chr(365)
+        return delimiter.join(strs)
+    
+    def decode(self, s: str) -> List[str]:
+        delimiter = chr(365)
+        return s.split(delimiter)
