5722f4a76a2e17ee49f2f94f0fdb6c3fce224b06
Muxi Jin <jmx1998@student.ubc.ca>
2024-06-08 15:46:30 -0700

    11 2nd try

--- Top Interview 150/11_Container_With_Most_Water.py
+++ Top Interview 150/11_Container_With_Most_Water.py
@@ -15,3 +15,17 @@ class Solution:
             else:
                 r-=1
         return max_v
+####### Sencond time attempt
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        l,r = 0,n-1
+        res = 0
+        while l<r:
+            res = max(res, min(height[l],height[r])*(r-l))
+            if height[l] < height[r]:
+                l+=1
+            else:
+                r-=1
+        return res
+
